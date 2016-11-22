6a3dcbca56c763d6ee53c4d60aa2b623e58fb3dd
hunan <nan.hu@51auto.com>
2016-11-22 15:36:26 +0800

    x

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PROJECT_NAME=test
-sodu docker build -t hunan/$PROJECT_NAME . && sodu docker run -d -P --name $PROJECT_NAME hunan/$PROJECT_NAME 
+sudo docker build -t hunan/$PROJECT_NAME . && sudo docker run -d -P --name $PROJECT_NAME hunan/$PROJECT_NAME 

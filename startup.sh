31d3a7fc2729456a827830dabfe82c21bff1072b
hunan <nan.hu@51auto.com>
2016-11-22 16:20:18 +0800

    x

--- startup.sh
+++ startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-sudo docker build -t hunan/test_web . && sudo docker run -d -p 8080:8080 --name test_web hunan/test_web
+
+IMAGE_NAME="hunan/test_web"
+CONTAINER_NAME="test_web"
+
+sudo docker build -t $IMAGE_NAME . 
+sudo docker run -d -p 8080:8080 --name $CONTAINER_NAME $IMAGE_NAME
+
+sudo docker attach $CONTAINER_NAME
+
+RC=$(sudo docker wait $CONTAINER_NAME)

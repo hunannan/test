9c6ff3cf147f8079cbd30009b8564f763f63939d
hunan <nan.hu@51auto.com>
2016-11-22 16:47:59 +0800

    x

--- startup.sh
+++ startup.sh
@@ -3,9 +3,25 @@
 IMAGE_NAME="hunan/test_web"
 CONTAINER_NAME="test_web"
 
+#是否有正在运行的container
+RUNNING_CONTAINER=$(sudo docker ps | grep $CONTAINER_NAME | awk '{print $NF}')
+
+if [ "$RUNNING_CONTAINER" ]
+	then
+	 sudo docker stop $CONTAINER_NAME
+	 echo "[Stop Container]:$CONTAINER_NAME ... "
+	 sudo docker rm $CONTAINER_NAME
+	 echo "[Delete Container]:$CONTAINER_NAME ... "
+fi
+
+#是否有之前构建的容器
+EXISTED_IMAGE=$(sudo docker images | grep $IMAGE_NAME | awk '{print $1}')
+if [ "$EXISTED_IMAGE" ]
+	then
+	 sudo docker rmi $EXISTED_IMAGE
+	 echo "[Delete IMAGE]:$EXISTED_IMAGE ... "
+fi
+
 sudo docker build -t $IMAGE_NAME . 
 sudo docker run -d -p 8080:8080 --name $CONTAINER_NAME $IMAGE_NAME
-
 sudo docker attach $CONTAINER_NAME
-
-RC=$(sudo docker wait $CONTAINER_NAME)
